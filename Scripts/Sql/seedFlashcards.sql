40772b4a76a5da08c41dd32b3f72a66843fafed0
Charlie Merrell <charlie.merrell20@gmail.com>
2021-12-13 12:45:42 +0000

    added sql seed script

---
+++ Scripts/Sql/seedFlashcards.sql
@@ -0,0 +1,4 @@
+-- SQLite
+-- sqlite> .read ./Scripts/Sql/seedFlashcards.sql
+INSERT INTO Flashcards (Prompt, CorrectAnswer, CreatedAt)
+VALUES ("What is the capital of England", "London", "2016-01-01 10:20:05.123");
